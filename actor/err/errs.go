cec1f3fe332a5c351654d6fb053d443154499bdb
wwj_29 <229482191@qq.com>
2021-05-31 23:16:58 +0800

    no message

--- actor/err/errs.go
+++ actor/err/errs.go
@@ -9,6 +9,7 @@ var (
 	RegisterEventErr = fmt.Errorf("RegistEvent only accept ptr param")
 	CancelEventErr   = fmt.Errorf("CancelEvent only accept ptr param")
 	DispatchEventErr = fmt.Errorf("DispatchEvent only accept ptr param")
+	EventHasNotErr   = fmt.Errorf("system event has not been set")
 
 	//error of actor system
 	ActorSystemOptionErr   = fmt.Errorf("actor system option run failed")
@@ -18,5 +19,4 @@ var (
 	RegisterActorSameIdErr = fmt.Errorf("actor with same id")
 	ActorNotFoundErr       = fmt.Errorf("local actor not found")
 	ActorPushMsgErr        = fmt.Errorf("push a nil msg to actor")
-	EventHasNotErr         = fmt.Errorf("system event has not been set")
 )
