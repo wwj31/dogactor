5e83bab211a85b799886b78298b4af304279e353
wwj31 <229482191@qq.com>
2022-08-07 17:14:56 +0800

    cli

--- dog-cli/cmd/root.go
+++ dog-cli/cmd/root.go
@@ -11,9 +11,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-//┏━┓
-//┃╋┃
-//┗━┛
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "dog-cli [command]",
