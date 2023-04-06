01e1ba62f3a9cfd555ccf34085dcb352628738b9
wwj <>
2023-04-06 16:35:16 +0800

    proto enum

--- tools/proto.go
+++ tools/proto.go
@@ -1,107 +1,94 @@
 package tools
 
 import (
-	"github.com/gogo/protobuf/proto"
-	"github.com/wwj31/dogactor/log"
 	"reflect"
 	"strings"
+
+	"github.com/gogo/protobuf/proto"
+
+	"github.com/wwj31/dogactor/log"
 )
 
-type GetProtoByName func(name string) (interface{},bool)
+type GetProtoByName func(name string) (interface{}, bool)
 type EnumIdx struct {
 	PackageName string
-	Enum2Name  map[int32]string
-	name2Enum map[string]int32
+	Prefix      string
+	Enum2Name   map[int32]string
+	Name2Enum   map[string]int32
 }
 
 type ProtoIndex struct {
 	structByName GetProtoByName
-	enum EnumIdx
+	enum         EnumIdx
 }
 
-func NewProtoIndex(f GetProtoByName,enum EnumIdx) *ProtoIndex {
-	pi :=  &ProtoIndex{
+func NewProtoIndex(f GetProtoByName, enum EnumIdx) *ProtoIndex {
+	pi := &ProtoIndex{
 		structByName: f,
-		enum: enum,
+		enum:         enum,
 	}
-	newName2Enum := make(map[string]int32,len(pi.enum.Enum2Name))
-	for e,name := range pi.enum.Enum2Name{
-		lowerName := convertMsgName(name)
-		newName := pi.enum.PackageName + "." + lowerName
-		pi.enum.Enum2Name[e] = newName
-		newName2Enum[newName] = e
-	}
-	pi.enum.name2Enum = newName2Enum
 	return pi
 }
 
 func (s *ProtoIndex) UnmarshalPbMsg(msgId int32, data []byte) proto.Message {
-	ptName, ok := s.enum.Enum2Name[msgId]
+	enumName, ok := s.enum.Enum2Name[msgId]
 	if !ok {
-		log.SysLog.Errorw("msg not regist parser", "msgId", msgId, "data", data)
+		log.SysLog.Errorw("the message not registry to the parser", "msgId", msgId, "data", data)
 		return nil
 	}
 
-	v,ok := s.structByName(ptName)
-	if !ok{
-		log.SysLog.Errorw("not found ptName", "msgId", msgId, "ptName", ptName)
+	if !strings.HasPrefix(enumName, s.enum.Prefix) {
+		log.SysLog.Errorw("the message has not prefix", "msgId", msgId, "enumName", enumName, "prefix", s.enum.Prefix)
+		return nil
+	}
+
+	_, ptName, _ := strings.Cut(enumName, s.enum.Prefix)
+	v, ok := s.structByName(ptName)
+	if !ok {
+		log.SysLog.Errorw("cannot find enumName", "msgId", msgId, "enumName", enumName)
 		return nil
 	}
 
 	msg := v.(proto.Message)
 	err := proto.Unmarshal(data, msg)
 	if err != nil {
-		log.SysLog.Errorw("msg parse failed", "msgId", msgId, "data", data)
+		log.SysLog.Errorw("the message parse failed", "msgId", msgId, "data", data)
 		return nil
 	}
 	return msg
 }
 
 func (s *ProtoIndex) MsgIdToName(msgId int32) (msgName string, ok bool) {
-	name,ok := s.enum.Enum2Name[msgId]
+	enumName, ok := s.enum.Enum2Name[msgId]
 	if !ok {
 		return
 	}
+	_, ptName, _ := strings.Cut(enumName, s.enum.Prefix)
 
-	return name, true
+	return ptName, ok
 }
 
 func (s *ProtoIndex) MsgNameToId(msgName string) (msgId int32, ok bool) {
-	id, ok := s.enum.name2Enum[msgName]
+	enumName := s.enum.Prefix + msgName
+	id, ok := s.enum.Name2Enum[enumName]
 	if !ok {
 		return
 	}
 	return id, true
 }
 
-func convertMsgName(msgName string) (name string) {
-	words := strings.Split(msgName, "_")
-	for _, word := range words {
-		lower := strings.ToLower(word)
-		if len(words) > 0{
-			runes := []rune(lower)
-			if 97 <= runes[0] && runes[0] <= 122{
-				runes[0] -= 32
-			}
-			lower = string(runes)
-		}
-		name += lower
-	}
-	return
-}
-
-func (s *ProtoIndex)MsgName(msg proto.Message) string {
+func (s *ProtoIndex) MsgName(msg proto.Message) string {
 	name := reflect.TypeOf(msg).String()
-	if len(name) > 0 && name[0] == '*'{
+	if len(name) > 0 && name[0] == '*' {
 		name = name[1:]
 	}
 	return name
 }
 
-func (s *ProtoIndex)FindMsgByName(msgName string) (proto.Message, bool) {
-	v,ok := s.structByName(msgName)
-	if !ok{
-		return nil,false
+func (s *ProtoIndex) FindMsgByName(msgName string) (proto.Message, bool) {
+	v, ok := s.structByName(msgName)
+	if !ok {
+		return nil, false
 	}
-	return v.(proto.Message),true
+	return v.(proto.Message), true
 }
